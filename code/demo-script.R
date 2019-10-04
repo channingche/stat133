6f8bdaf9c0932dbac7ee813b6d8653866429a579
Channing Che <channingche@berkeley.edu>
2019-10-04 16:14:27 -0700

    adding first version of demo-script

---
+++ code/demo-script.R
@@ -0,0 +1,13 @@
+# ================================================
+# Title: Descriptive Analysis
+# Description: 
+#   This script computes descriptive statistics, as well as 
+#   various exploratory data visualizations.
+# Input(s): data file "nba2018-players.csv"
+# Output(s): summary data files and plots
+# Author: Channing Che
+# Date: 10-03-2019
+# ================================================
+# packages
+library(dplyr)
+library(ggplot2)
